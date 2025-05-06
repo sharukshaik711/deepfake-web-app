6567f6d0aa0e249b9745024844858b4c013e1e0d
sharuk823 <sharukshaik823@gamil.com>
2025-05-06 15:08:32 +0530

    update gunicorn setup and flask appimprovements

--- web_app/app.py
+++ web_app/app.py
@@ -5,29 +5,33 @@ from predict import predict_video
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024  # Optional: 50MB upload limit
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    if 'video' not in request.files:
-        return "No video uploaded", 400
+    if request.method == 'POST':
+        if 'video' not in request.files:
+            return "No video uploaded", 400
 
-    video = request.files['video']
-    filename = secure_filename(video.filename)
-    path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    video.save(path)
+        video = request.files['video']
+        if video.filename == '':
+            return "Empty filename", 400
 
-    prediction, avg_conf, frames = predict_video(path)
+        filename = secure_filename(video.filename)
+        path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        video.save(path)
 
-    return render_template('result.html', 
-                           filename=filename,
-                           prediction=prediction,
-                           confidence=round(avg_conf, 4),
-                           frames=frames)
+        prediction, avg_conf, frames = predict_video(path)
 
-if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=10000)
+        return render_template('result.html',
+                               filename=filename,
+                               prediction=prediction,
+                               confidence=round(avg_conf, 4),
+                               frames=frames)
+    else:
+        return redirect(url_for('index'))
